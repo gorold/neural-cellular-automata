8a4e3a011b182cf54b08afdf7aa668888c5a63e4
soohmatthew <matthewsoh18@gmail.com>
2020-08-11 16:46:59 +0800

    Updated GUI

--- run_GUI.py
+++ run_GUI.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
-import random
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 import os
 import numpy as np
 import torch
 from PIL import Image
-
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import cv2
 import matplotlib.pyplot as plt
 
-from utils import make_seed, to_rgb, to_rgba
-from NeuralCellularAutomata import GrowingNCA
+from utils import make_seed, to_rgb, load_emoji_dict, pad_target
+from NeuralCellularAutomata import GrowingNCA, ConditionalNCA
+
+global device 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class Model(object):
-    def __init__(self, *args, **kwargs):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        #device = torch.device('cpu')
-        self.model = GrowingNCA(device = device)
-        self.model.load_state_dict(torch.load(f'{os.getcwd()}/models/nca_0.pth'))
+    def __init__(self, conditional = False, target = None):
+        if conditional:
+            self.model = ConditionalNCA(device = device)
+            self.model.load_state_dict(torch.load(f'{os.getcwd()}/models/nca_1.pth'))
+        else:
+            self.model = GrowingNCA(device = device)
+            self.model.load_state_dict(torch.load(f'{os.getcwd()}/models/nca_{target}.pth'))
         self.x = make_seed((56, 56), 1, 16).to(device)
 
 class GUI(tk.Frame):
@@ -26,7 +29,7 @@ class GUI(tk.Frame):
         # Initialise frame
         tk.Frame.__init__(self, *args, **kwargs)
         
-        # Some global variables
+        # Some necessary variables
         self.interval_speed = tk.IntVar() # Varable for interval speed, initial speed = 1x
         self.interval_speed.set(1)
         self.mouse_clicked = False
@@ -34,14 +37,20 @@ class GUI(tk.Frame):
         self.modelType = tk.StringVar() # Variable for model type
         self.modelType.set("conditional")
 
+        self.conditional_target = {k: pad_target(v) for k, v in load_emoji_dict('data/train').items()}
+            
         self.modelTarget = tk.StringVar()
         self.modelTarget.set("Select Model Target")
         
+        self.current_modelType = "conditional"
+        self.current_modelTarget = "Select Model Target"
+
         self.scale_labels = {}
         for i in range(1,6):
             self.scale_labels[i] = f"Speed: {i}x ({i * 2} iterations/s)" 
         
         # Add widgets
+
         # Radio Button to choose type of model
         tk.Label(master= root, text = 'Please choose the type of model:').grid(row=0, column=0, sticky = 'NW')        
         self.conditional_button = ttk.Radiobutton(master = root, text="Conditional Model", variable=self.modelType,
@@ -58,63 +67,116 @@ class GUI(tk.Frame):
 
         # Generate Button
         self.generate_button = tk.Button(master=root, text="Generate", command=self.generate_model)
-        self.generate_button.grid(row=4, column= 1)
+        self.generate_button.grid(row=5, column= 0, sticky = 'W')
+
+        # Refresh Button
+        self.refresh_button = tk.Button(master=root, text = 'Refresh', command = self.refresh_x, state=tk.DISABLED)
+        self.refresh_button.grid(row = 5, column = 1, sticky = 'W')
 
         # Speed Scale
         self.scale = tk.Scale(master= root, from_=1, to= 5, command=self.onScale, orient = tk.HORIZONTAL, tickinterval= 1, length=400, showvalue = 0, label = self.scale_labels[1])
-        self.scale.grid(row=6, column = 0)
+        self.scale.grid(row=7, column = 0)
 
         # Quit Button
         self.quit_button = tk.Button(master=root, text="Quit", command=self._quit)
-        self.quit_button.grid(row=6, column= 1)
+        self.quit_button.grid(row=7, column= 1)
 
-        # Plot matplotlib canvas
-        self.fig = plt.figure(figsize=(5, 5), dpi=100)
+        # Plot initial README canvas and set up options
+        self.fig = plt.figure(figsize=(5, 5), dpi=150)
         self.fig.add_subplot(111)
-        image = Image.open(r'C:\Users\Dell\Desktop\neural-cellular-automata\data\train\emoji_u1f62f.png').convert('RGB') # Replace with instructions
+        image = Image.open(f'{os.getcwd()}/assets/readme.png').convert('RGB') # Replace with instructions
         plt.imshow(image)
         plt.axis('off')
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
-        self.canvas.get_tk_widget().grid(row=5, columnspan = 2)
-        
-        # Update plot every x interval, depending on the user's speed
+        self.canvas.get_tk_widget().grid(row=6, columnspan = 2)
         self.change_conditional()
 
     def change_conditional(self):
+        # Changes the dropdown box options to show the relevant options
         self.modelTarget.set("Select Model Target")
-        self.select_model_target['values'] = ['1', '2']
+        self.select_model_target['values'] = list(self.conditional_target.keys())
+
     def change_normal(self):
+        # Changes the dropdown box options to show the relevant options
         self.modelTarget.set("Select Model Target")
-        self.select_model_target['values'] = ['3', '4']    
+        self.select_model_target['values'] = ['0', '2', '3']   
 
     def onScale(self, val):
+        # Set speed of scale
         v = int(val)
         self.interval_speed.set(v)
         self.scale.config(label=self.scale_labels[v])
 
     def update_plot(self):
+        # Updates plot by passing through the current image (x) into the NCA model
         self.fig.clear()
         self.nca.model.eval()
         with torch.no_grad():
-            self.nca.x = self.nca.model(self.nca.x)
-        out = np.transpose(to_rgb(self.nca.x).detach().cpu().numpy()[0], (1, 2, 0))
+            if self.current_modelType == 'conditional':
+                self.nca.x = self.nca.model(self.nca.x, None, self.conditional_encoding[self.current_modelTarget])
+                out = np.transpose(to_rgb(self.nca.x).detach().cpu().numpy()[0], (1, 2, 0))
+            else:
+                self.nca.x = self.nca.model(self.nca.x)
+                out = np.transpose(to_rgb(self.nca.x).detach().cpu().numpy()[0], (1, 2, 0))
         plt.imshow((out * 255).astype(np.uint8))
         plt.axis('off')
         self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=5, columnspan=3)
+        self.canvas.get_tk_widget().grid(row=6, columnspan=3)
         self.after(int(100/self.interval_speed.get()), self.update_plot) # how often you want to refresh
 
     def generate_model(self):
+        # Function called when "generate" button is pressed
         model_type = self.modelType.get()
         model_target = self.modelTarget.get()
-        
-        self.nca = Model()
-        self.canvas.callbacks.connect('button_press_event', self.on_click)
-        self.canvas.callbacks.connect('motion_notify_event', self.drag)
-        self.canvas.callbacks.connect('button_release_event', self.release)
-        self.canvas.draw()
-        self.update_plot()
 
+        self.current_modelType = model_type
+        self.current_modelTarget = model_target
+
+        # If invalid selection, show error message, prompt user to choose correct option and regenerate
+        if model_target == 'Select Model Target':
+            self.fig.clear()
+            image = Image.open(f'{os.getcwd()}/assets/error.png').convert('RGB')
+            plt.axis('off')
+            plt.imshow(image)
+            self.canvas.draw()
+            self.refresh_button['state'] = tk.DISABLED
+
+        # If correct option, present loading screen while model is loading, subsequently, show model.
+        else:
+            self.fig.clear()
+            image = Image.open(f'{os.getcwd()}/assets/loading.png').convert('RGB')
+            plt.axis('off')
+            plt.imshow(image)
+            self.canvas.draw()
+            
+            if model_type == 'conditional':
+                self.nca = Model(conditional = True)
+                self.conditional_encoding = {}
+                for k, v in self.conditional_target.items():
+                    self.conditional_encoding[k] = self.nca.model.get_encoding(v.unsqueeze(0).to(device))
+            else:
+                self.nca = Model(target = model_target)
+
+            self.refresh_button['state'] = tk.NORMAL
+            self.canvas.callbacks.connect('button_press_event', self.on_click)
+            self.canvas.callbacks.connect('motion_notify_event', self.drag)
+            self.canvas.callbacks.connect('button_release_event', self.release)
+            self.canvas.draw()
+            self.update_plot()
+
+    def refresh_x(self):
+        # Tries to refresh the x, otherwise show an error message
+        try:
+            self.nca.x = make_seed((56, 56), 1, 16).to(device)
+        except:
+            self.fig.clear()
+            image = Image.open(f'{os.getcwd()}/assets/error.png').convert('RGB')
+            plt.axis('off')
+            plt.imshow(image)
+            self.canvas.draw()
+            self.refresh_button['state'] = tk.DISABLED
+
+    # Some functions to white out a small circle of the image when clicked on
     def on_click(self, event):
         if event.inaxes is not None:
             self.mouse_clicked = True
@@ -131,24 +193,23 @@ class GUI(tk.Frame):
     def release(self,event):
         self.mouse_clicked = False
 
+    # Fully stops function
     def _quit(self):
         root.quit()
         root.destroy()
 
+# Fully stops function
 def on_closing():
     if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
         root.quit()
         root.destroy()
 
 if __name__ == "__main__":
-    import time
-    start = time.time()
-    
     root = tk.Tk()
     GUI(root)
-    root.geometry("500x650")
+    root.geometry("700x950")
+    root.wm_title("Neural Cellular Automata")
+    root.iconbitmap(f"{os.getcwd()}/assets/emoji_u1f44c.ico")
     root.resizable(width=False, height=False)
     root.protocol("WM_DELETE_WINDOW", on_closing)
-    end = time.time()
-    print(end - start)
     root.mainloop()
